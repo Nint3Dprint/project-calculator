23c411f7419ab08edd9300fa3f091aab8a60da42
Ada Lin <nint3dprint@gmail.com>
2026-08-08 12:09:04 -0400

    Made UI look better and added ^2 and ^3.

--- first-row-buttons.js
+++ first-row-buttons.js
@@ -10,7 +10,7 @@ function clearDisplay() {
 
 function equalsShowAnswer() {
     const myInput = document.getElementById("display-box");
-var result = myInput.value.replace("\u00f7", "/").replace("π", "Math.PI").replace(/\u221a(\d+)/g, 'Math.sqrt($1)');
+var result = myInput.value.replace("\u00f7", "/").replace("π", "Math.PI").replace(/\u221a(\d+\.\d+)/g, 'Math.sqrt($1)').replace(/(\d+\.*\d*)\u00b2/g , 'Math.pow($1, 2)').replace(/(\d+\.*\d*)\u00b3/g , 'Math.pow($1, 3)')
 console.log(result);
     myInput.value = eval(result);
 }

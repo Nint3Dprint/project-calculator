0ee154167b13c800fb1cf211d2cb8bfb8d2f89fb
Ada Lin <nint3dprint@gmail.com>
2026-08-07 20:20:27 -0400

    changed layout, square root doesn't work.

--- first-row-buttons.js
+++ first-row-buttons.js
@@ -2,3 +2,25 @@ function buttonFunctions(input) {
     const myInput = document.getElementById("display-box");
     myInput.value = myInput.value + input;
 }
+
+function clearDisplay() {
+    const myInput = document.getElementById("display-box");
+    myInput.value = "";
+}
+
+function equalsShowAnswer() {
+    const myInput = document.getElementById("display-box");
+var result = myInput.value.replace("\u00f7", "/").replace("π", "Math.PI").replace("\u221a\d+", "Math.sqrt()");
+console.log(result);
+    myInput.value = eval(result);
+}
+
+function plusMinus() {
+    const myInput = document.getElementById("display-box");
+    myInput.value = myInput.value * -1;
+}
+
+function backspace() {
+    const myInput = document.getElementById("display-box");
+    myInput.value = myInput.value.slice(0, -1);
+}
